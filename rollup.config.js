0a3e8eadade2cdcf004ab57e11e17bd4c579bcdb
Marcos Aguayo <marcosaguayomora@gmail.com>
2022-03-16 18:50:18 +0100

    Use master as package

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
+import resolve from '@rollup/plugin-node-resolve'
 import { uglify } from 'rollup-plugin-uglify'
 import autoprefixer from 'autoprefixer'
 
